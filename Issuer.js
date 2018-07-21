a1b846ad789029a18ea5af4713d5a86bc57fa4bc
WorkLaptop <Jmillenaar@vxcompany.com>
2018-07-21 14:52:44 +0200

    Test

---
+++ Issuer.js
@@ -0,0 +1,11 @@
+//Includes
+const IOTA = require("iota.lib.js");
+const MAM = require('mam.client.js');
+
+//Create the connection
+let IOTA_Object = new IOTA({
+  'provider': 'https://nodes.testnet.iota.org:443'
+});
+//Claim
+
+//Revoke
