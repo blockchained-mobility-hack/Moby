7f2652b6cdca469fc1bca2add4107ebeeac85ace
WorkLaptop <Jmillenaar@vxcompany.com>
2018-07-22 13:30:10 +0200

    Validaty

--- src/ValidatyCheck.js
+++ src/ValidatyCheck.js
@@ -3,33 +3,40 @@ const IOTA = require("iota.lib.js");
 const MAM = require('./mam.client.js');
 const Wrapper = require('./IOTA_Implementation.js');
 
-let Arg = process.argv;
-let ClaimHash = Arg[2];
-let ClaimAddress = Arg[3];
-let UserAddress = Arg[4];
-let Data = ValidatyCheck(Arg[2], Arg[3], Arg[4]);
-console.log( Data);
+//let Arg = process.argv;
+//let ClaimHash = Arg[2];
+//let ClaimAddress = Arg[3];
+//let UserAddress = Arg[4];
+//let Data = ValidatyCheck(Arg[2], Arg[3], Arg[4]);
+
+//Data.then((Result) => {
+//  console.log(Result);
+//});
+
 function ValidatyCheck(ClaimHash, ClaimAddress, UserAddress) {
-  let UserMAM = new Wrapper.MAM_Publisher(Wrapper.IOTA_Object, Wrapper.UserSeed, 2, Wrapper.PRIVACYLEVEL.public);
-  UserMAM.CatchupChannel();
-  let MamReader = new Wrapper.MAM_Reader(ClaimAddress, Wrapper.PRIVACYLEVEL.public);
+  return new Promise(function(fulfill, reject)   {
+    let UserMAM = new Wrapper.MAM_Publisher(Wrapper.IOTA_Object, Wrapper.UserSeed, 2, Wrapper.PRIVACYLEVEL.public);
+    UserMAM.CatchupChannel();
+    let MamReader = new Wrapper.MAM_Reader(ClaimAddress, Wrapper.PRIVACYLEVEL.public);
 
-  //Transaction data
-  let PromTransactions = MamReader.FetchStream();
+    //Transaction data
+    let PromTransactions = MamReader.FetchStream();
 
-  PromTransactions.then((Result) => {
-    let ClaimStatus = Wrapper.GetClaimStatus(UserAddress, Result );
-    if(ClaimStatus[1] == "Revoked") {
-      console.log("Claim has been revoked!");
-      return false;
-    } else if(ClaimStatus[0] == ClaimHash) {
-      console.log("Claimhash matches!");
-      return true;
-    }
-    return true;
+    PromTransactions.then((Result) => {
+      let ClaimStatus = Wrapper.GetClaimStatus(UserAddress, Result );
+      if(ClaimStatus[1] == "Revoked") {
+        console.log("Claim has been revoked!");
+        fulfil(false);
+      } else if(ClaimStatus[0] == ClaimHash) {
+        console.log("Claimhash matches!");
+        fulfill(true);
+      }
+      fulfill(true);
+    });
+
+    PromTransactions.catch((Result) => {
+      reject(Result);
+    });
   });
 
-  PromTransactions.catch((Result) => {
-    console.log(Result);
-  });
 }
